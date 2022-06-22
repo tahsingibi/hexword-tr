399c3c60ab24091d58781c5f97ac15650dccb6a6
Tahsin Sungur <m.tahsinsungur@gmail.com>
2022-06-22 20:01:02 +0300

    add github

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header() {
         <h1 className="text-4xl">#HEXWORDS-TR</h1>
         <p>#5EFFAF olmayı seçebilecekken neden rastgele bir yeşille uğraşasın!</p>
         <p className="text-black text-opacity-50 text-sm py-1">
-            by <b><a href="https://sungur.dev/">tahsingibi</a></b> ~ <a href="https://github.com/tahsingibi/hexword-tr">github</a>• &nbsp;
+            by <b><a href="https://sungur.dev/">tahsingibi</a></b> ~ <a href="https://github.com/tahsingibi/hexword-tr">github</a> • &nbsp;
             <a href="https://hexwords.netlify.app/">hexwords-en</a> •
             teşekkürler: <b><a href="https://twitter.com/fkadev">fkadev</a></b>
         </p>
